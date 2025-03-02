d5ff88da3e7b98db9687f1bec617f0dd4bb036da
ailinbaudin <ailinbaudin@gmail.com>
2025-03-02 15:27:03 -0300

    edit vote develop

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,22 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
 process.stderr.write = (chunk) => {
     if (chunk.includes('Deprecation Warning')) {
         return;
     }
 };
-
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? '/Poke-App/' : '/', 
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
     build: {
         outDir: 'dist',
         sourcemap: false,
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom',
     },
 });
